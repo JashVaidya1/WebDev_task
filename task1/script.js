ded9272560f249a8d3f162381ce10657365a7f95
JashVaidya1 <jashvaidya1@gmail.com>
2024-05-18 18:14:20 +0530

    Add files via upload

---
+++ task1/script.js
@@ -0,0 +1,43 @@
+
+
+function on_1() {
+    document.getElementById("overlay_1").style.display = "block";
+  }
+  
+  function off_1() {
+      document.getElementById("overlay_1").style.display = "none";
+    }
+
+  function on_2() {
+    document.getElementById("overlay_2").style.display = "block";
+  }
+  
+  function off_2() {
+    document.getElementById("overlay_2").style.display = "none";
+  }
+  
+  function on_3() {
+    document.getElementById("overlay_3").style.display = "block";
+}
+  
+  function off_3() {
+    document.getElementById("overlay_3").style.display = "none";
+}
+
+  function on_4() {
+    document.getElementById("overlay_4").style.display = "block";
+  }
+  
+  function off_4() {
+      document.getElementById("overlay_4").style.display = "none";
+    }
+    
+
+
+
+    
+    
+    
+    
+    
+    
